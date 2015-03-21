fb768c612040ef33511a5fea825f7872ee93fc04
Tocknicsu <wingemerald@gmail.com>
2015-03-21 14:28:59 +0800

    Add forgot password button

--- backend/register.py
+++ backend/register.py
@@ -7,7 +7,7 @@ class RegisterService:
 
     def signup(self, email, password, repassword):
         if password != repassword:
-            return ('Epassword', None)
+            return ('請再次確認密碼', None)
         def _hash(p):
             ret = 1048576
             MOD = 10000000007
@@ -20,13 +20,13 @@ class RegisterService:
         yield cur.execute('SELECT * FROM "account" '
                 'WHERE "account"."email" = %s;', (email, ))
         if cur.rowcount != 0:
-            return ('Eexist', None)
+            return ('此Email已被註冊', None)
         yield cur.execute('INSERT INTO "account" '
                 '( "email", "password") '
                 'VALUES ( %s ,%s) '
                 'RETURNING "account"."uid";', (email, _hash(password)))
         if cur.rowcount != 1:
-            return ('Edb', None)
+            return ('註冊失敗 請聯絡管理員', None)
         uid = str(cur.fetchone()[0])
         return (None, str(uid))
 
@@ -44,7 +44,7 @@ class RegisterHandler(RequestHandler):
             password = str(self.get_argument('password'))
             repassword = str(self.get_argument('repassword'))
         except:
-            self.finish('E')
+            self.finish('未知錯誤 請聯絡管理員')
             return
         err, uid = yield from RegisterService.inst.signup(email, password, repassword)
         if err:
