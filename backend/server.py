83d4e06101ebeada1425e138b2b5d725fab591e8
Tocknicsu <wingemerald@gmail.com>
2015-03-21 10:45:16 +0800

    MenuBar Prototype complete

--- backend/server.py
+++ backend/server.py
@@ -5,6 +5,8 @@ import config
 from req import RequestHandler
 from req import reqenv
 from req import Service
+from login import LoginHandler
+from login import LoginService
 
 class IndexHandler(RequestHandler):
     @reqenv
@@ -16,6 +18,7 @@ if __name__ == '__main__':
     db = pg.AsyncPG(config.DBNAME, config.DBUSER, config.DBPASSWORD, dbtz='+8')
     app = tornado.web.Application([
         ('/', IndexHandler),
+        ('/login', LoginHandler),
         ('/(.*)', tornado.web.StaticFileHandler, {'path': '../html'}),
         ], cookie_secret=config.COOKIE_SECRET, autoescape='xhtml_escape')
     app.listen(config.PORT)
