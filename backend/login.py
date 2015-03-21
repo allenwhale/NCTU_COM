48ddf3a7d2810075adb6eb885d4f60024b56e680
allenwhale <allenwhale@yahoo.com.tw>
2015-03-21 10:55:59 +0800

    add register

--- backend/login.py
+++ backend/login.py
@@ -5,18 +5,6 @@ class LoginService:
         self.db = db
         LoginService.inst = self
 
-    def signup(self, name, first_name, last_name, gender, degree, country, affiliation, department, position, affiliation_postcode, affiliation_address, contact_postcode, contact_address, email, password, repassword, ability):
-        cur = yield self.db.cursor()
-        yield cur.execute('SELECT 1 FROM "account" '
-                'WHERE "account"."name" = %s OR "account"."email" = %s;', (name, email))
-        if cur.rowcount != 0:
-            return ('Eexist', None)
-        yield cur.execute('INSERT INTO "sccount" '
-                '("name", "first_name", "last_name", "gender", "degree", "country", '
-                '"affiliation", "department", "affiliation_postcode", "affiliation_address", '
-                '"contact_postcode", "contact_address", "email", "password") '
-                'VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s ,%s) '
-                'RETURNING "account"."uid";', (name, first_name, last_name, gender, degree, country, affiliation, affiliation_postcode, affiliation_address, contact_postcode, contact_address, email, password))
 
 class LoginHandler(RequestHandler):
     @reqenv
@@ -26,26 +14,5 @@ class LoginHandler(RequestHandler):
 
     @reqenv
     def post(self):
-        try:
-            name = str(self.get_argument('name'))
-            first_name = str(self.get_argument('first_name'))
-            last_name = str(self.get_argument('last_name'))
-            gender = str(self.get_argument('gender'))
-            degree = str(self.get_argument('degree'))
-            country = str(self.get_argument('country'))
-            affiliation = str(self.get_argument('affiliation'))
-            department = str(self.get_argument('department'))
-            position = str(self.get_argument('position'))
-            affiliation_postcode = str(self.get_argument('affiliation_postcode'))
-            affiliation_address = str(self.get_argument('affiliation_address'))
-            contact_postcode = str(self.get_argument('contact_postcode'))
-            contact_address = str(self.get_argument('contact_address'))
-            cellphone = str(self.get_argument('cellphone'))
-            tellphone = str(self.get_argument('tellphone'))
-            password = str(self.get_argument('password'))
-            repassword = str(self.get_argument('repassword'))
-            ability = str(self.get_arguments('ability'))
-        except:
-            self.finish('E')
-            return
+        pass
 
