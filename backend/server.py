45c3af4b314fc3852897c5bc14fa83682cdec588
Tocknicsu <wingemerald@gmail.com>
2015-03-21 09:55:36 +0800

    fixed server.py

--- backend/server.py
+++ backend/server.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     db = pg.AsyncPG(config.DBNAME, config.DBUSER, config.DBPASSWORD, dbtz='+8')
     app = tornado.web.Application([
         ('/', IndexHandler),
-        ('/(.*)', tornado.web.StaticFileHandler, {'path': '../http'}),
+        ('/(.*)', tornado.web.StaticFileHandler, {'path': '../html'}),
         ], cookie_secret=config.COOKIE_SECRET, autoescape='xhtml_escape')
     app.listen(config.PORT)
     tornado.ioloop.IOLoop().instance().start()
