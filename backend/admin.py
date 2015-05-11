c3831c531eab41c466efa2ec573002268344b21c
allenwhale <allenwhale@yahoo.com.tw>
2015-05-11 11:18:22 +0800

    add admin&user reply

--- backend/admin.py
+++ backend/admin.py
@@ -13,8 +13,48 @@ class AdminService:
         return 1 if int(acct['uid']) in  config.ADMIN_RANGE else 0
 
     def admin_reply(self, pid, f):
+        if not f:
+            return ('Efile', None)
         cur = yield self.db.cursor()
-       # yield cur.execute('SELECT "paper"')
+        yield cur.execute('SELECT "papercheck", "status" FROM "paperupload" WHERE "pid" = %s;', (pid,))
+        if cur.rowcount != 1:
+            return ('Epaper', None)
+        check, status = cur.fetchone()
+        check = int(check)
+        status = int(status)
+        if status != 0:
+            return ('Eturn', None)
+        path = '../html/paper/'+str(pid)+'/reply-'+str(pid)+('' if check == 0 else '%d'%check)+'.'+f['filename'].split('.')[-1]
+        _f = open(path, 'wb')
+        _f.write(f['body'])
+        _f.close()
+        yield cur.execute('UPDATE "paperupload" SET "status" = 1 WHERE "pid"= %s;', (pid,)) 
+        if cur.rowcount != 1:
+            return ('Edb', None)
+        return (None, pid)
+
+    def user_reply(self, pid, f):
+        if not f:
+            return ('Efile', None)
+        cur = yield self.db.cursor()
+        yield cur.execute('SELECT "papercheck", "status" FROM "paperupload" WHERE "pid" = %s;', (pid,))
+        if cur.rowcount != 1:
+            return ('Epaper', None)
+        check, status = cur.fetchone()
+        check = int(check)
+        status = int(status)
+        if status != 1:
+            return ('Eturn', None)
+        path = '../html/paper/'+str(pid)+'/rreply-'+str(pid)+('' if check == 0 else '%d'%check)+'.'+f['filename'].split('.')[-1]
+        _f = open(path, 'wb')
+        _f.write(f['body'])
+        _f.close()
+       
+        yield cur.execute('UPDATE "paperupload" SET "status" = 1, "papercheck" = %s WHERE "pid"= %s;', (check+1, pid,)) 
+        if cur.rowcount != 1:
+            return ('Edb', None)
+        return (None, pid)
+
 
 
 
