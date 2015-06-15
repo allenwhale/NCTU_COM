6481f6fde13dcc4a76938a37bf208c0e1036276e
root <root@cdmr>
2015-06-16 00:45:55 +0800

    fixed

--- backend/paperupload.py
+++ backend/paperupload.py
@@ -12,11 +12,15 @@ class PaperuploadService:
 
     def upload(self, uid, chinesetitle, englishtitle, chineseabstract, englishabstract, letter, picnum, wordnum, submitted, confirm, conflict, conflict_explain, anony_file, non_anony_file, author, chinesekeywords, englishkeywords):
 #            , chineseabstract, englishabstract, chinesekeywords, englishkeywords, authors, letter, picnum, wordnum, submitted, confirm, conflict, conflict_explain, attach_file):
-        if chinesetitle == '' or englishtitle == '' or chineseabstract == '' or englishabstract == '' or letter == '' or picnum == '' or wordnum == '' or conflict == '':
+        if chinesetitle == '' or englishtitle == '' or chineseabstract == '' or englishabstract == '' or letter == '' or picnum == '' or wordnum == '' :
+            print(1)
             return ('Eempty', None)
         if chinesekeywords.count('') == 5 or englishkeywords.count('') == 5:
+            print(2)
             return ('Eempty', None)
+        print(confirm)
         if confirm.count('0') != 0:
+            print(3)
             return ('Eempty', None)
         cur = yield self.db.cursor()
         yield cur.execute('INSERT INTO "paperupload" ("uid", "chinesetitle", "englishtitle", "chineseabstract", "englishabstract", "letter", "picnum", "wordnum", "submitted", "confirm", "conflict", "conflict_explain") VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) RETURNING "paperupload"."pid";', (uid, chinesetitle, englishtitle, chineseabstract, englishabstract, letter, picnum, wordnum, submitted, confirm, conflict, conflict_explain));
